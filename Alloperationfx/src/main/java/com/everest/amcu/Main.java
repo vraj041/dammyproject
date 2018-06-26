96fab5a5d8453f01687f76fd0e5b5e5460bc1692
vraj041 <38758943+vraj041@users.noreply.github.com>
2018-06-26 15:29:25 +0530

    remove stage from master
    last change remove

--- Alloperationfx/src/main/java/com/everest/amcu/Main.java
+++ Alloperationfx/src/main/java/com/everest/amcu/Main.java
@@ -18,7 +18,7 @@ public class Main extends Application {
 			Scene scene = new Scene(root);
 			// scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-				primaryStage.setTitle("LAST CHANGE");
+			
 			primaryStage.show();
 		} catch (Exception e) {
 			e.printStackTrace();
