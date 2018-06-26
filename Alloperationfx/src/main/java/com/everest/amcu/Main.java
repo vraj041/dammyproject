828f8c019b7b276ebc85d767846500c87d4aae69
vraj041 <38758943+vraj041@users.noreply.github.com>
2018-06-26 15:18:58 +0530

    change in branch
    last change in vraj branch

--- Alloperationfx/src/main/java/com/everest/amcu/Main.java
+++ Alloperationfx/src/main/java/com/everest/amcu/Main.java
@@ -18,7 +18,7 @@ public class Main extends Application {
 			Scene scene = new Scene(root);
 			// scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-				primaryStage.setTitle("change on github ok done");
+				primaryStage.setTitle("change last in vraj branch");
 			primaryStage.show();
 		} catch (Exception e) {
 			e.printStackTrace();
