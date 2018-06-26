4b629023f86ac055c882f1133ed6e736f84100b5
vraj041 <38758943+vraj041@users.noreply.github.com>
2018-06-26 16:10:12 +0530

    title set
    title of App

--- Alloperationfx/src/main/java/com/everest/amcu/Main.java
+++ Alloperationfx/src/main/java/com/everest/amcu/Main.java
@@ -18,8 +18,8 @@ public class Main extends Application {
 			Scene scene = new Scene(root);
 			// scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			
-			primaryStage.show();
+		    primaryStage.setTitle("title of App");
+            primaryStage.show();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
